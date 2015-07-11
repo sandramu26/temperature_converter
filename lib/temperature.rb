523ba47a2af1910b9e6c544ce65b8a7b4351b87a
sandramu26 <san.muva@hotmail.com>
2015-07-10 21:25:03 -0500

    Temperate completed

--- lib/temperature.rb
+++ lib/temperature.rb
@@ -1,2 +1,22 @@
 class Temperature
+
+	def initialize(temp)
+                @temp = temp
+        end
+	
+	def self.in_fahrenheit(temp)				
+		obj_f  = Temperature.new(temp)				
+	end
+
+	def self.in_celsius(temp)
+		obj_c  = Temperature.new(temp)	
+	end
+	
+	def to_far
+		resul = ((@temp * 1.8) + 32)
+	end
+
+	def to_cel
+		resul = ((@temp - 32) / 1.8)
+	end
 end
